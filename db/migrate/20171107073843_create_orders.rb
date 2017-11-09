8d1de8768cf88bad770871ec15b333d46882785f
Yuri Lima <Yuri Lima>
2017-11-09 07:50:08 -0200

    go

--- db/migrate/20171107073843_create_orders.rb
+++ db/migrate/20171107073843_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.references :ad, index: true, foreign_key: true
+      t.references :ad, index: false, foreign_key: true
       t.integer :status, default: 0
       t.references :member, index: true, foreign_key: true
 
